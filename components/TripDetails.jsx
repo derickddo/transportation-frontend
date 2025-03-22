a80052bc366e9994224e27fd51341ca3364f56f7
derickddo <dansoderrick80@yahoo.com>
2025-03-22 19:23:46 +0000

    Update API_URL in components and configure rewrites in vercel.json for backend integration

--- components/TripDetails.jsx
+++ components/TripDetails.jsx
@@ -6,7 +6,7 @@ import { FaMapMarkerAlt, FaClock, FaRoute, FaInfoCircle, FaCalendarDay, FaTimes 
 import Sidebar from "./Sidebar"; // Import the Sidebar component
 
 
-const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const API_URL = "/api"
 
 const TripDetails = () => {
   const { tripId } = useParams();
